941ad8f8ff1db77ce667a72802864650c49d465c
25LP6514 25LP6514 <25LP6514@G302-CC0111.local>
2025-10-01 18:10:40 +0800

    hi

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      <button>add</button>
+     </div>
+    </>
+  )
+}
+
+export default App
